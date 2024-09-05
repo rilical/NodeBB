3756f1f152e15b403329afe439c30f4869cfce79
e_oghabayen <oghabayen.ext@creativeadvtech.com>
2024-09-05 08:53:24 -0400

    Refactor categorySearch.js to reduce nesting and improve readability

--- test/categorySearch.js
+++ test/categorySearch.js
@@ -1,7 +1,5 @@
 'use strict';
 
-console.log('OMAR_GHABAYEN: categorySearch called');
-
 const assert = require('assert');
 const { JSDOM } = require('jsdom');
 
@@ -9,56 +7,71 @@ const { window } = new JSDOM('<!doctype html><html><body></body></html>');
 const $ = require('jquery')(window);
 
 // Mock the required modules
-const alerts = {
-	error: () => {},
-};
+const alerts = { error: () => {} };
 const api = {
 	get: (url, params, callback) => {
-		callback(null, { categories: [] });
+		console.log('API called with:', url, params);
+		callback(null, { categories: [{ cid: 1, name: 'Test Category' }] });
 	},
 };
-
-// Mock the `utils` module to keep things simple for testing
+const bootstrap = { Dropdown: { getInstance: () => ({ update: () => {} }) } };
 const utils = {
-	params: () => ({}),
-	debounce: fn => fn, // No real debounce needed for test
+	debounce: fn => fn,
 	findBootstrapEnvironment: () => 'lg',
+	params: () => ({}),
 };
-
-// Mock `ajaxify` and `app`
-global.ajaxify = {
-	data: {
-		selectedCids: [],
-		selectedCategory: null,
-		allCategoriesUrl: '/categories',
-	},
-};
-
-global.app = {
+const app = {
 	parseAndTranslate: (template, data, callback) => {
-		callback($('<div><ul component="category/list"></ul></div>'));
+		console.log('parseAndTranslate called with:', template, data);
+		callback($('<div><ul component="category/list"><li>Test Category</li></ul></div>'));
 	},
 };
 
-// Load the module under test
-const categorySearch = require('../public/src/modules/categorySearch');
+const ajaxify = {
+	data: {
+		template: {},
+		selectedCids: [],
+		selectedCategory: {},
+		allCategoriesUrl: '',
+	},
+};
+
+const categorySearchFactory = require('../public/src/modules/categorySearch');
+
+$.fn.html = function (content) {
+	if (content !== undefined) {
+		console.log('jQuery html method called with:', content);
+		this.empty().append(content);
+	}
+	return this;
+};
+
+const categorySearch = categorySearchFactory(alerts, bootstrap, api, utils, app, $, ajaxify);
 
 describe('Category Search', () => {
 	let el;
 
 	beforeEach(() => {
-		// Set up a mock DOM element with jQuery
-		el = $('<div><input component="category-selector-search"/></div>');
+		el = $('<div><div component="category-selector-search"><input type="text"/></div><div component="category/list"></div></div>');
+		$('body').empty().append(el);
 	});
 
-	it('should initialize with default options', () => {
-		const options = {}; // Empty options object
+	it('should handle search input and render results', (done) => {
+		const options = { template: 'some-template' };
 		categorySearch.init(el, options);
+		el.trigger('show.bs.dropdown');
+		const searchInput = el.find('input');
+		assert.strictEqual(searchInput.length, 1, 'Search input not found');
+		searchInput.val('test');
+		searchInput.trigger('keyup');
 
-		// Assert default options are applied
-		assert.strictEqual(options.privilege, 'topics:read', 'Default privilege should be topics:read');
-		assert.deepEqual(options.states, ['watching', 'tracking', 'notwatching', 'ignoring'], 'Default states should be set');
-		assert.strictEqual(options.cacheList, true, 'Default cacheList should be true');
+		setTimeout(() => {
+			console.log('DOM after timeout:', $('body').html());
+			const categoryList = $('[component="category/list"]');
+			console.log('Category list:', categoryList.length, categoryList.html());
+			assert.strictEqual(categoryList.find('li').length, 1, 'Category item not found');
+			assert.strictEqual(categoryList.find('li').text(), 'Test Category', 'Incorrect category text');
+			done();
+		}, 500);
 	});
-	// USED CHATGPT 4.0
 });
